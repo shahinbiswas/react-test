fa28d1f4291910cd227b0cb8ba4d00dc8d41f7b9
shahinul-islam <shahinulislam8397@gmail.com>
2023-01-07 19:42:18 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM.render(<h1>Todo App</h1>, document.getElementById("root"));
+
+const TodoTitle = "Call Family";
+const TodoDes =
+  "Tempor amet ipsum labore rebum dolores tempor, duo eirmod vero";
+const date = new Date();
+const dateName = date.getDate();
+// const monthName = date.monthName();
+const currentYear = date.getFullYear();
+
+// const headingStyle ={
+//     color:"green",
+//     fontSize:"3rem",
+//     backgroundColor:"purple",
+//     textAlign:"center",
+//     padding:"15px"
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <h1 className="headingStyle">Todo App</h1>
+    <div className="card">
+      <h3 className="cardTitle">{TodoTitle}</h3>
+      <p className="cardDesc">{TodoDes}</p>
+      <p className="cardFooter">{dateName + " / " + currentYear}</p>
+    </div>
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
